06ccbbc0f6eb39352ba003325c3a5d4f7e11f84b
Yuto Umeda <yt.d.rep@gmail.com>
2022-05-20 23:11:03 +0900

    test

--- docker/mongo/init.js
+++ docker/mongo/init.js
@@ -15,3 +15,20 @@ db.createCollection("post", {});
 db.user.createIndex({"location": "2d"});
 
 db.user.find({location: {$near: [139.684011, 35.740095], $minDistance: 0, $maxDistance: 10}})
+
+db.places.insertMany([
+    {
+        "name": "Tokyo Station",
+        "location": {
+            "type": "Point",
+            "coordinates": [139.76732, 35.681246]
+        }
+    },
+    {
+        "name": "Ikebukuro Station",
+        "location": {
+            "type": "Point",
+            "coordinates": [139.711214, 35.729888]
+        }
+    },
+])
